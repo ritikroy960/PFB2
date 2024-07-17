a9a1b23d67bb193065ba4e1e2f672a5380116114
ritikroy960 <132186112+ritikroy960@users.noreply.github.com>
2024-07-17 10:51:09 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,14 +1,14 @@
 import os
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN")
-    APP_ID = int(os.environ.get("APP_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
-    CHANNEL1_ID = os.environ.get("CHANNEL1_ID")
-    CHANNEL1_NAME = os.environ.get("CHANNEL1_NAME")
-    CHANNEL2_ID = os.environ.get("CHANNEL2_ID")
-    CHANNEL2_NAME = os.environ.get("CHANNEL2_NAME")
+    TG_BOT_TOKEN = os.environ.get("6380142667:AAGXWxV3Fg8MW8V8zIHb6tqREaMe9FsXNms")
+    APP_ID = int(os.environ.get("21347819"))
+    API_HASH = os.environ.get("3d9e35374cc5aa394c123dd8062b3469")
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "2133811513").split())
+    CHANNEL1_ID = os.environ.get("-1001827229996")
+    CHANNEL1_NAME = os.environ.get("nino_reward")
+    CHANNEL2_ID = os.environ.get("-1001992332883")
+    CHANNEL2_NAME = os.environ.get("anshuman_loot")
     CHANNEL3_ID = os.environ.get("CHANNEL3_ID")
     CHANNEL3_NAME = os.environ.get("CHANNEL3_NAME")
     CHANNEL4_ID = os.environ.get("CHANNEL4_ID")
